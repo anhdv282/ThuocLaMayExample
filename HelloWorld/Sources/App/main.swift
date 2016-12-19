8b04dc9c4bfe118209d22b131ad2d771f5e6cdf2
vietanh <vietanh@emotiv.vn>
2016-12-19 16:05:33 +0700

    Init

---
+++ HelloWorld/Sources/App/main.swift
@@ -0,0 +1,28 @@
+import Vapor
+
+let drop = Droplet()
+
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
+}
+
+drop.get("/") { req in
+        return "Hello World!"
+}
+
+drop.get("/view") { req in
+    return try drop.view("Hello World!")
+}
+
+drop.get("/name",":name") { request in
+    if let name = request.parameters["name"]?.string {
+        return "Hello \(name)!"
+    }
+    return "Error retrieving parameters"
+}
+
+drop.resource("posts", PostController())
+
+drop.run()
